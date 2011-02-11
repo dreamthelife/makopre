71edff0f014188557b85275a2d71e0aca972fc6b
Andrea Lattuada <tuagle@xps-laptop.(none)>
2011-02-11 01:24:25 +0100

    basic tests and implementation

--- src/makopre.py
+++ src/makopre.py
@@ -1,5 +1,9 @@
 #! /usr/bin/python
 
+# Makopre - a small binary to preprocess text using mako
+#
+# Copyright 2011 Andrea Lattuada <utaaal@gmail.com>
+
 import argparse
 
 ARGS = None
@@ -8,30 +12,52 @@ def ParseArgs():
   parser = argparse.ArgumentParser()
   def arg(*v, **vv):
     parser.add_argument(*v, **vv)
-  arg('files', nargs='+')
-  arg('--output', required=True)
+  arg('--output', default='out/',
+      help='Path to the directory the preprocessed files are stored into.')
+  arg('--debug', default=False, action='store_const', const=True,
+      help='Enable debug level logging.')
   global ARGS
   ARGS = parser.parse_args()
 
 import logging
 import mako.template
+import os
 import os.path
 import sys
 
 def main():
   ParseArgs()
-  for filename in ARGS.files:
-    try:
-      template = mako.template.Template(filename=filename)
-    except IOError:
-      logging.error('Cannot open file %s.', filename)
-      continue
-    rendered = template.render()
-    try:
-      out_path = os.path.join(ARGS.output, filename)
-      with open(out_path, 'w') as output_file:
-        output_file.write(rendered)
-    except IOError:
-      logging.error('Cannot open output file %s for writing.', out_path)
+  if ARGS.debug:
+    logging.basicConfig(level=logging.DEBUG)
+  logging.info("Walking directory tree")
+  for dirpath, dirnames, filenames in os.walk(os.curdir):
+    logging.info("Entering directory %s", dirpath)
+    output_dir_path = ARGS.output
+    if dirpath != '.':
+      output_dir_path = os.path.join(ARGS.output, dirpath)
+    if not os.path.isdir(output_dir_path):
+      logging.info("Creating output directory %s", output_dir_path)
+      os.mkdir(output_dir_path)
+    for filename in filenames:
+      rel_file_path = os.path.join(dirpath, filename)
+      try:
+        with open(rel_file_path, 'r') as input_file:
+          line1 = input_file.readline()
+          if line1.strip() != '## makopre':
+            logging.debug("Skipping file %s", rel_file_path)
+            continue
+          input_text = input_file.read()
+      except IOError:
+        logging.error('Cannot open file %s', rel_file_path)
+        continue
+      logging.info("Processing file %s", filename)
+      template = mako.template.Template(input_text)
+      rendered = template.render()
+      try:
+        out_path = os.path.join(ARGS.output, rel_file_path)
+        with open(out_path, 'w') as output_file:
+          output_file.write(rendered)
+      except IOError:
+        logging.error('Cannot open output file %s for writing.', out_path)
 
 main()
---
+++ src/makopre_test.py
@@ -0,0 +1,51 @@
+# Copyright 2011 Andrea Lattuada <utaaal@gmail.com>
+
+import unittest
+
+import filecmp
+import logging
+import subprocess
+import os.path
+import shutil
+import sys
+import tempfile
+
+TMP_DIR = os.path.join(tempfile.gettempdir(), 'makopreout')
+TEST_FILE_SIMPLE = 'template.txt'
+
+ARGS = ['../../makopre.py', '--debug', '--output', TMP_DIR]
+
+def setUp():
+  logging.info('Output path: %s', TMP_DIR)
+
+class TestBinary(unittest.TestCase):
+  def setUp(self):
+    logging.debug(os.getcwd())
+    self.restored_dir = os.getcwd()
+    if os.path.isdir(TMP_DIR):
+      shutil.rmtree(TMP_DIR)
+    os.mkdir(TMP_DIR)
+
+  def tearDown(self):
+    os.chdir(self.restored_dir)
+
+  def CallMakopre(self):
+    p = subprocess.Popen(ARGS, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    for l in p.stdout.readline():
+      logging.info(l)
+    return p.wait()
+
+  def test_simple_invocation(self):
+    os.chdir('testdata/simple')
+    self.assertEqual(0, self.CallMakopre())
+    with open(os.path.join(TMP_DIR, TEST_FILE_SIMPLE)) as output_file:
+      self.assertEqual('Hello, world!\n', output_file.read())
+
+  def test_dirtree_invocation(self):
+    os.chdir('testdata/dirtree')
+    self.assertEqual(0, self.CallMakopre())
+    self.assert_(os.path.isfile(os.path.join(TMP_DIR, 'root.txt')))
+    self.assert_(os.path.isfile(os.path.join(TMP_DIR, 'dir/leaf.txt')))
+
+if __name__ == '__main__':
+  unittest.main()
